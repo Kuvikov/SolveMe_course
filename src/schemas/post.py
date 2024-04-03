bd6b049ca50f920d24aaec075d53f871a4c2cd92
Kuvikov <pillarskp@gmail.com>
2024-04-03 21:43:28 +0300

    Отделил проверки

--- src/schemas/post.py
+++ src/schemas/post.py
@@ -1,4 +1,11 @@
 POST_SCHEMA = {
-
-    
+    "type": "object",
+    "properties": {
+        "id": {"type": "number"},
+        "body": {"type": "string"},
+        "postId": {"type": "number"},
+    },
+    "required": ["body"]
 }
+
+#{'id': 1, 'body': 'some comment', 'postId': 1}
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,15 +1,14 @@
 import requests
 
 from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
-from jsonschema import validate
+from src.schemas.post import POST_SCHEMA
+from src.baseclasses.response import Response
 
 def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-    response_posts = response.json()
-    print(response_posts)
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    assert len(response_posts) == 2, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
-    validate(response_posts)
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+
+    response.assert_status_code(200).validate(POST_SCHEMA)
+    
 
 ## [{'id': 1, 'body': 'some comment', 'postId': 1}, {'id': 2, 'body': 'some comment', 'postId': 1}]
