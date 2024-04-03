353b9b7a8bc9a6e45eab9ac069edd35e1fa100f1
Kuvikov <pillarskp@gmail.com>
2024-04-03 20:57:22 +0300

    json

--- tests/test_something.py
+++ tests/test_something.py
@@ -2,11 +2,14 @@ import requests
 
 from configuration import SERVICE_URL
 from src.enums.global_enums import GlobalErrorMessages
-
+from jsonschema import validate
 
 def test_getting_posts():
     response = requests.get(url=SERVICE_URL)
     response_posts = response.json()
-
+    print(response_posts)
     assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
     assert len(response_posts) == 2, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+    validate(response_posts)
+
+## [{'id': 1, 'body': 'some comment', 'postId': 1}, {'id': 2, 'body': 'some comment', 'postId': 1}]
